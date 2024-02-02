e3a2645683020b2c5821dae82a206abcd34405ba
Jaime Garcia Marquez <jrgarcia.marquez@gmail.com>
2024-02-02 15:34:30 +0100

    updates

--- dev_create_roi_table.sh
+++ dev_create_roi_table.sh
@@ -1,65 +1,84 @@
 #! /bin/bash
 
+
+#####  INPUTS
+
 #  path to env tables
 export ENVTB=/data/marquez/vignette/env_tables
 
 # path, including the file, of the subcatchment for the roi
 export SUBC=/mnt/shared/sosw/tmp/danube_subcatchments.tif
 
-# output folder
-export OUT=/data/marquez/vignette/out
+# output file
+export OUTFILE=/data/marquez/vignette/out/projectionTB.csv
+
+# file with the list of subcatchments IDs
+export SUBIDS=/data/marquez/vignette/out/subc_IDs.txt
+
+# folder for temporal files
+export TMP=/data/marquez/vignette/out
+
+# path to file with list of variables (1st row) and tiles (2nd row)
+export VT=$TMP/var_tiles.txt
 
 
-grass -f --text --tmp-location $SUBC
 
-r.in.gdal --o input=$SUBC output=subcat
-r.describe -1 -n subcat > $OUT/subc_IDs.txt
+##### ANALYSIS
 
+# variables of interest
+#export var=( bio1 c10_2020 spi stright )
+export var=( awk 'NR == 1' $VT )
 
-# identify tiles
-tiles=$(ls $ENVTB | awk -F_ '{print $1}' | sort | uniq )
+# tiles of interest
+#export tiles=( h18v02 h18v04 h20v02 h20v04 )
+export tiles=( awk 'NR == 2' $VT )
 
-# identify variables
-var=( $(ls $ENVTB | awk -F[_.] '{print $2}' | sort | uniq ) )
-
-# extract the data for the variable of interest for the list of subcatchments
+# extract the data for the variables of interest for the list of subcatchments
 # of interest for all tiles
-for TL in $tiles
+for TL in ${tiles[@]}
 do
     for k in ${var[@]}
     do
     awk 'NR==FNR {a[$1]; next} FNR==1 || $1 in a' \
-     $OUT/subc_IDs.txt $ENVTB/${TL}_${k}.txt \
-     >  $OUT/ENV_${TL}_${k}.txt
+     $SUBCIDS $ENVTB/${TL}_${k}.txt \
+     >  $TMP/ENV_${TL}_${k}.txt
     done
 done
 
-
-################
 ### join tables of different tiles for same variable
-variables=( $(find $OUT -name "ENV_*.txt" | awk -F[_.] '{print $3}' \
-    | sort | uniq) )
+echo ${var[@]} | xargs -n 1 -P 1 bash -c $'
 
-# the chunk below assumes the mean and sd were extracted
-# add if statement for Land Cover data
-echo ${variables[@]} | xargs -n 1 -P 2 bash -c $'
 X=$1
-echo "subCid ${X}_min ${X}_max ${X}_range ${X}_mean ${X}_sd" > $OUT/ENVaggreg_${X}.txt
-cat $(find $OUT -name "ENV_*_${X}.txt") >> $OUT/ENVaggreg_${X}.txt
-sed \'/subcID/d\' $OUT/ENVaggreg_${X}.txt > $OUT/ENVaggreg_${X}f.txt
-sort -g $OUT/ENVaggreg_${X}f.txt > $OUT/ENVaggreg_${X}.txt
-rm $OUT/ENVaggreg_${X}f.txt 
+
+listf=( $(find $TMP -name "ENV_*_${X}.txt")  )
+
+cols=$( awk \'NR==1 {print NF}\' ${listf[0]} )
+
+if [[ "$cols" -eq 6 ]]
+then
+    echo "subCid ${X}_min ${X}_max ${X}_range ${X}_mean ${X}_sd" > $TMP/ENVaggreg_${X}.txt
+    cat ${listf[@]} >> $TMP/ENVaggreg_${X}.txt
+    sed \'/subcID/d\' $TMP/ENVaggreg_${X}.txt > $TMP/ENVaggreg_${X}f.txt
+    sort -g $TMP/ENVaggreg_${X}f.txt > $TMP/ENVaggreg_${X}.txt
+    rm $TMP/ENVaggreg_${X}f.txt
+else
+    echo "subCid ${X}" > $TMP/ENVaggreg_${X}.txt
+    cat ${listf[@]} >> $TMP/ENVaggreg_${X}.txt
+    sed \'/subcID/d\' $TMP/ENVaggreg_${X}.txt > $TMP/ENVaggreg_${X}f.txt
+    sort -g $TMP/ENVaggreg_${X}f.txt > $TMP/ENVaggreg_${X}.txt
+    rm $TMP/ENVaggreg_${X}f.txt
+fi
+
 ' _
 
-paste -d" " $(find $OUT/ENVaggreg_*.txt) > $OUT/ENVpred.txt
+## join all the environmental variables together
+paste -d" " $(find $TMP/ENVaggreg_*.txt) > $TMP/ENVpred.txt
 
-################
+
 ###  join tables of all variables together
 
-
 ## Chunk to delete the subCid column for each table before joining
-
-read -a header < $OUT/ENVpred.txt # read first line into array "header"
+read -a header < $TMP/ENVpred.txt # read first line into array "header"
 declare -a arr=() # array to store the position in which the subCid name is
 
 for i in ${!header[@]}               # iterate through array indexes
@@ -73,14 +92,17 @@ done
 printf -v joined '%s,' "${arr[@]}"
 
 paste -d"," \
-    <(printf "%s\n" subcID $(awk 'NR > 1 {print $1}' $OUT/ENVpred.txt)) \
-    <(tr -s ' ' ',' < $OUT/ENVpred.txt | \
+    <(printf "%s\n" subcID $(awk 'NR > 1 {print $1}' $TMP/ENVpred.txt)) \
+    <(tr -s ' ' ',' < $TMP/ENVpred.txt | \
     cut -d"," --complement -f $(echo "${joined%,}")) \
-    > $OUT/danube_predict_md.csv
+    > $TMP/projectionTBtmp.csv
 
 #### remove duplicates
-awk -F, '!a[$0]++'  $OUT/danube_predict_md.csv > $OUT/danube_predict.csv
+awk -F, '!a[$0]++'  $TMP/projectionTBtmp.csv > $OUTFILE
 
 # remove temporal files
-rm $OUT/ENV*.txt  
+rm $TMP/projectionTBtmp.csv
+rm $TMP/ENV*.txt  
+rm $VT
 
+exit
