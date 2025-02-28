a34e49ec26cbb05746436056b8049e0e30c9c832
Jaime Garcia Marquez <jrgarcia.marquez@gmail.com>
2025-02-28 18:12:41 +0100

    new additions sdm

--- sosw_danube.sh
+++ sosw_danube.sh
@@ -1,64 +1,98 @@
 #! /bin/bash
 
 ### STEP 1: download to a folder the environmental variables of interest
+ENV=/mnt/shared/Environment90m_v.1.0_online/
 
 TILE=(h18v02 h18v04 h20v02 h20v04)
 
 for TL in ${TILE[@]}
 do
-
-	for file in $(find /mnt/shared/Environment90m_v.1.0_online/LandCover/ -name "*_2020_${TL}.zip")
+    for var in c60 c80 
 	do 
-	sudo unzip -j $file -d /mnt/shared/danube/env_tl
+    file=$(find $ENV/esa_cci_landcover_v2_1_1/${var} -name "${var}_2020_${TL}.zip")
+	sudo unzip -j $file -d /mnt/shared/sosw/danube/env
 	done
 
-	for file in $(find /mnt/shared/Environment90m_v.1.0_online/Climate/present -name "*_${TL}.zip")
+    for var in bio01 bio12 bio04 bio15 
 	do 
-	sudo unzip -j $file -d /mnt/shared/danube/env_tl
+    file=$(find $ENV/chelsa_bioclim_v2_1/1981-2010_observed/${var} -name "${var}_*_${TL}.zip")    
+	sudo unzip -j $file -d /mnt/shared/sosw/danube/env
+	done
+    
+    for var in bio1 bio12 bio4 bio15
+    do 
+    file=$(find $ENV/chelsa_bioclim_v2_1/2071-2100_projected/${var} -name "${var}_*_ukesm1-0-ll_ssp585_*_${TL}.zip")    
+	sudo unzip -j $file -d /mnt/shared/sosw/danube/env
 	done
 	
-	for file in $(find /mnt/shared/Environment90m_v.1.0_online/Hydrography90m -name "*_${TL}.zip")
+    for var in flow length slope_grad_dw_cel
 	do 
-	sudo unzip -j $file -d /mnt/shared/danube/env_tl
-	done
-	
-	for file in $(find /mnt/shared/Environment90m_v.1.0_online/Soil -name "*_${TL}.zip")
-	do 
-	sudo unzip -j $file -d /mnt/shared/danube/env_tl
+    file=$(find $ENV/hydrography90m_v1_0/${var} -name "${var}_${TL}.zip")    
+	sudo unzip -j $file -d /mnt/shared/sosw/danube/env
 	done
 	
 done
 
+
 ### STEP 2:  Create the master table for the area of interest
 
-bio=( $(ls /mnt/shared/Environment90m_v.1.0_online/Climate/present) )
 
-#hyd=( $(ls /mnt/shared/Environment90m_v.1.0_online/Hydrography90m) )
-
-hyd=( chancurv chandistdwseg chandistupcel chandistupseg chanelvdwcel chanelvdwseg chanelvupcel chanelvupseg changraddwseg changradupcel changradupseg elev_drop flow flow_accum flowpos gradient length out_dist out_drop outdiffdwbasin outdiffdwscatch outdistdwbasin outdistdwscatch outlet_elev slopdiff slopgrad  source_elev strdiffdwnear strdiffupfarth strdiffupnear strdistdwnear strdistprox strdistupfarth strdistupnear stright )
-
-soil=( soil_ACDWRB soil_AWCtS soil_BDRICM soil_BDRLOG soil_BLDFIE soil_CECSOL soil_CLYPPT soil_CRFVOL soil_HISTPR soil_ORCDRC soil_PHIHOX soil_SLGWRB soil_SLTPPT soil_SNDPPT soil_TEXMHT soil_WWP )
-
-cob=( c10_2020 c20_2020 c30_2020 c40_2020 c50_2020 c60_2020 c70_2020 c80_2020 c90_2020 c100_2020 c110_2020 c120_2020 c130_2020 c140_2020 c150_2020 c160_2020 c170_2020 c180_2020 c190_2020 c200_2020 c210_2020 c220_2020 )
-
-ENV=( ${bio[@]} ${hyd[@]} ${soil[@]} ${cob[@]} )
+# present variables
+ENV=( c60_2020 c80_2020 bio01_1981-2010_observed bio12_1981-2010_observed bio04_1981-2010_observed bio15_1981-2010_observed flow length slope_grad_dw_cel)
 printf -v joined '%s,' "${ENV[@]}"
 
-#### !!!!!!!!!!!!!!!!!!!!   check if the function works with the new names of the files!!!!!!!!
-
-time  bash /mnt/shared/danube/dev_create_roi_table.sh \
+time  bash /mnt/shared/sosw/dev_create_roi_table.sh \
   $(echo "${joined%,}") \
-  mean,sd \
+  mean \
   h18v02,h18v04,h20v02,h20v04 \
-  /mnt/shared/danube/env_tl  \
-  /mnt/shared/danube/subc_IDs.txt  \
-  /mnt/shared/danube/out/danube_predictTB.csv  \
+  /mnt/shared/sosw/danube/env  \
+  /mnt/marquez/marquez/vignette/data/subc_IDs.txt  \
+  /mnt/shared/sosw/danube/danube_predictTB.csv  \
   /mnt/shared/tmp/danube \
-  20
+  25
+
+# future variables
+FUT=(  bio12_2071-2100_ukesm1-0-ll_ssp585_V.2.1 bio15_2071-2100_ukesm1-0-ll_ssp585_V.2.1 bio1_2071-2100_ukesm1-0-ll_ssp585_V.2.1 bio4_2071-2100_ukesm1-0-ll_ssp585_V.2.1 )
+printf -v joined '%s,' "${FUT[@]}"
+
+
+time  bash /mnt/shared/sosw/dev_create_roi_table.sh \
+  $(echo "${joined%,}") \
+  mean \
+  h18v02,h18v04,h20v02,h20v04 \
+  /mnt/shared/sosw/danube/env  \
+  /mnt/marquez/marquez/vignette/data/subc_IDs.txt  \
+  /mnt/shared/sosw/danube/danube_predictFT.csv  \
+  /mnt/shared/tmp/danube \
+  16
 
 ### Check resullts
-awk -F, '{print NF}' /mnt/shared/danube/out/danube_predictTB.csv | sort | uniq
+#awk -F, '{print NF}' /mnt/shared/sosw/danube/danube_predictTB.csv | sort | uniq -c
 
+####  change the name of the bio climate variables for the present and future
+
+sed 's/bio01_1981-2010_observed_mean/bio1/g' $DIR/sdm/danube_predictTB.csv > $TMP/predTB_tmp2.csv
+sed 's/bio04_1981-2010_observed_mean/bio4/g' $TMP/predTB_tmp2.csv > $TMP/predTB_tmp3.csv
+sed 's/bio12_1981-2010_observed_mean/bio12/g' $TMP/predTB_tmp3.csv > $TMP/predTB_tmp4.csv
+sed 's/bio15_1981-2010_observed_mean/bio15/g' $TMP/predTB_tmp4.csv > $DIR/sdm/danube_predictTB.csv
+
+rm $TMP/predTB_tmp*.csv 
+
+sed 's/bio1_2071-2100_ukesm1-0-ll_ssp585_V.2.1_mean/bio1/g' $DIR/sdm/danube_predictFT.csv > $TMP/predTB_tmp2.csv
+sed 's/bio4_2071-2100_ukesm1-0-ll_ssp585_V.2.1_mean/bio4/g' $TMP/predTB_tmp2.csv > $TMP/predTB_tmp3.csv
+sed 's/bio12_2071-2100_ukesm1-0-ll_ssp585_V.2.1_mean/bio12/g' $TMP/predTB_tmp3.csv > $TMP/predTB_tmp4.csv
+sed 's/bio15_2071-2100_ukesm1-0-ll_ssp585_V.2.1_mean/bio15/g' $TMP/predTB_tmp4.csv > $DIR/sdm/danube_predictFT.csv
+
+rm $TMP/predTB_tmp*.csv 
+
+# prepare the predction table for the future by adding the missing columns
+
+paste -d"," \
+    $DIR/sdm/danube_predictFT.csv \
+    <(awk -F, 'BEGIN{OFS=","} {print $6, $7, $8, $9, $10}' $DIR/sdm/danube_predictTB.csv) \
+    > $DIR/sdm/danube_predictFT2.csv
+
+mv $DIR/sdm/danube_predictFT2.csv $DIR/sdm/danube_predictFT.csv 
 
 
 
@@ -145,67 +179,213 @@ paste -d"," $DIR/subTB.csv \
     <(awk -F, 'BEGIN{OFS=",";} {print $2, $3}' $DIR/sub_stats_disch19.csv) \
     > $DIR/predTBdanube.csv
 
+################################################################################
 #############    STEP 4:   create the tables per species
 
+export DIR=/mnt/shared/sosw/danube
 
 ### file with species list:
-splst=/mnt/shared/danube/species_list.csv
-awk -F, 'NR > 1 {print $1}' $splst > $DIR/spp_list.txt
+splst=$DIR/fish/species_list.csv
+awk -F, 'NR > 1 {print $1}' $splst > $DIR/fish/spp_list.txt
 
 ### database with species ocuurence data
-occ=/mnt/shared/danube/fish_danube.csv
-
-spp="Sander lucioperca"
-
-awk -F, -v SPP="${spp}" 'BEGIN{OFS=",";}  $2 == SPP {print $2, $3, $4}' $occ > $DIR/spp1.csv 
+#occ1=$DIR/fish/fish_danube.csv
+sed 's/\"//g' $DIR/fish/danube_records_final_04-11-2024.csv > $DIR/fish/danube4all.csv
+occ=$DIR/fish/danube4all.csv
 
 
- time bash dev_create_model_table.sh  \
-   $DIR/spp1.csv \
-   $DIR/predTBdanube.csv \
-   /mnt/shared/sosw/tmp/danube_subcatchments.tif  \
-   10000  \
-   /mnt/shared/danube/tmp  \
-   /mnt/shared/sosw/bio_modelling/danube/spp_input/spp_table.csv
-
-for i in $(seq 1 $(wc -l <  $DIR/spp_list.txt))
+for i in $(seq 1 $(wc -l <  $DIR/fish/spp_list.txt))
 do
-    spp=$(awk -v ROW="${i}" 'NR == ROW' $DIR/spp_list.txt)
+    spp=$(awk -v ROW="${i}" 'NR == ROW' $DIR/fish/spp_list.txt)
     spfnm=$(echo $spp | awk 'BEGIN{OFS="_";}{print $1, $2}')
+
+    cat \
+        <(echo "species,lon,lat") \
+        <(awk -F, -v SPP="${spp}" 'BEGIN{OFS=",";}  $1 == SPP && $2 > 0 {print $1, $3, $4}' $occ) \
+        > $DIR/fish/${spfnm}_tmp.csv
     
-    echo "species,X,Y" > /mnt/shared/danube/tmp/${spfnm}_tmp.csv
-    awk -F, -v SPP="${spp}" 'BEGIN{OFS=",";}  $2 == SPP {print $2, $3, $4}' $occ \
-    >> /mnt/shared/danube/tmp/${spfnm}_tmp.csv 
-
-bash dev_create_model_table.sh  \
-   /mnt/shared/danube/tmp/${spfnm}_tmp.csv \
-   $DIR/predTBdanube.csv \
-   /mnt/shared/sosw/tmp/danube_subcatchments.tif  \
+bash $DIR/../dev_create_model_table.sh  \
+   $DIR/fish/${spfnm}_tmp.csv \
+   $DIR/sdm/danube_predictTB.csv \
+   /mnt/marquez/marquez/vignette/data/subcatchments.tif  \
    10000  \
-   /mnt/shared/danube/tmp  \
-   /mnt/shared/sosw/bio_modelling/danube/spp_input/${spfnm}.csv
+   /mnt/shared/tmp/danube  \
+   $TMP/${spfnm}_tmp1.csv
+
+#sed 's/bio01_1981-2010_observed_mean/bio1/g' $TMP/${spfnm}_tmp1.csv > $TMP/${spfnm}_tmp2.csv
+#sed 's/bio04_1981-2010_observed_mean/bio4/g' $TMP/${spfnm}_tmp2.csv > $TMP/${spfnm}_tmp3.csv
+#sed 's/bio12_1981-2010_observed_mean/bio12/g' $TMP/${spfnm}_tmp3.csv > $TMP/${spfnm}_tmp4.csv
+#sed 's/bio15_1981-2010_observed_mean/bio15/g' $TMP/${spfnm}_tmp4.csv > $DIR/sdm/spp_input/${spfnm}.csv
+
+rm $TMP/${spfnm}_*.csv
+rm $DIR/fish/${spfnm}_tmp.csv
 
 done
 
 
+#mkdir $DIR/cog_layers
+#
+## runs also on server3
+#OUT=$DIR/cog_layers
+#
+#for basin in jucar danube rhein mekong
+#do
+#    mkdir $OUT/${basin}
+#
+#    for FILE in $DIR/bio_modelling/${basin}/randomForest_output/*.tif ; do
+#        filename="${FILE##*/}"
+#
+#        gdaladdo -r average $FILE 2 4 6 8 10 12 14 16 18 20 22 24 26 28 32 64 128
+#
+#        ### create cloud-optimised tif
+#        gdal_translate $FILE   $DIR/cog_layers/${basin}/COG_${filename}  \
+#            -co TILED=YES -co COPY_SRC_OVERVIEWS=YES -co COMPRESS=LZW 
+#    done
+#done
 
 
-mkdir $DIR/cog_layers
 
-# runs also on server3
-OUT=$DIR/cog_layers
 
-for basin in jucar danube rhein mekong
+#################################
+#################################
+#######   R CODE TO RUN RANDOM FOREST
+
+
+library(dplyr)
+library(ranger)
+library(hydrographr)
+library(modEvA)
+library(PresenceAbsence)
+
+
+folder_path <- "/mnt/shared/sosw/danube/sdm/randomForest_out/"
+
+# Predict table path
+pred_tb_c = read.csv("/mnt/shared/sosw/danube/sdm/danube_predictTB.csv")
+pred_tb_f = read.csv("/mnt/shared/sosw/danube/sdm/danube_predictFT.csv")
+
+nmc = names(pred_tb_c[,c(2:10)])
+nmf = names(pred_tb_f[,c(2:10)])
+
+
+all_species = read.table("/mnt/shared/sosw/danube/fish/species_to_model.txt")
+
+
+#for (i in 2:dim(all_species)[1])  {
+#for (i in c(2:9,24,25)){
+for (i in c(10:23,26:39)){
+
+    sp = strsplit(all_species[i,1], split=c("\\."))[[1]][1]  
+  # Record the start time
+  start <- Sys.time() 
+
+    env_data_pa = read.csv(paste0("/mnt/shared/sosw/danube/sdm/spp_input/",
+sp, ".csv"))
+    env_data_pa = env_data_pa[rowSums(is.na(env_data_pa)) == 0,]
+
+    data_train = env_data_pa
+
+  # number of presence records:
+  pres_num <- as.numeric(table(data_train$PresAbs)["1"])
+  sample_size <- c("0" = pres_num / nrow(data_train),
+                   "1" = pres_num / nrow(data_train))
+  
+  # convert the response to factor for RF model to return probabilities
+  data_train$PresAbs <- as.factor(data_train$PresAbs)
+  
+  # train model
+  cat(paste0("Train model for ",sp, "\n") )
+  model <- NULL
+  model <- ranger(data_train$PresAbs ~ .,
+                  data = data_train[,nmc],
+                  num.trees = 1000,
+                  mtry= 6,
+                  replace = T,
+                  sample.fraction = sample_size,
+                  oob.error = T,
+                  keep.inbag = T,
+                  num.threads = 4,
+                  importance ='impurity',
+                  probability = T)
+  
+  # predict
+  cat(paste0("Prediction for ",sp, "\n"))
+  predc <- predict(model, data = pred_tb_c[,nmc])
+  predf <- predict(model, data = pred_tb_f[,nmf])
+  
+  # extract predictions and make a data frame with sub-catchment IDs
+  predictionc <- data.frame("subcID" = pred_tb_c$subcID,
+                           "p" = as.numeric(predc$predictions[,2]))
+  predictionf <- data.frame("subcID" = pred_tb_f$subcID,
+                           "p" = as.numeric(predf$predictions[,2]))
+  
+  
+  # Multiply the probability values by 100, to convert them to integers
+  prediction4mapc <- predictionc |>
+    mutate(p = as.integer(round(predictionc$p, 2) * 100))
+
+  outc = paste0("/mnt/shared/sosw/danube/sdm/reclassTB/",sp,"_c.txt")  
+  write.table(prediction4mapc, outc, row.names=FALSE, col.names=FALSE)
+ 
+  prediction4mapf <- predictionf |>
+    mutate(p = as.integer(round(predictionf$p, 2) * 100))
+  
+  outf = paste0("/mnt/shared/sosw/danube/sdm/reclassTB/",sp,"_f.txt")  
+  write.table(prediction4mapf, outf, row.names=FALSE, col.names=FALSE)
+}
+
+################################################################################
+
+
+##############
+#######   reclassification of predictions
+
+sudo grass --text /mnt/shared/sosw/grassdata/danube/PERMANENT
+
+dir=/mnt/shared/sosw/danube
+rcl=$dir/sdm/reclassTB
+out=$dir/sdm/randomForest_out
+
+#r.in.gdal --o input=/mnt/shared/sosw/danube/gis/msk_danube.tif output=maskt
+
+paths=( $(ls $rcl/*_[cf].txt) )
+#tiff=${paths[0]}
+
+for tiff in ${paths[@]}
 do
-    mkdir $OUT/${basin}
 
-    for FILE in $DIR/bio_modelling/${basin}/randomForest_output/*.tif ; do
-        filename="${FILE##*/}"
+    spp=$(basename $tiff .txt)
+    [[ -f $out/${spp}_prob.tif ]] && continue
+    outn=$(echo ${spp}_prob.tif)
 
-        gdaladdo -r average $FILE 2 4 6 8 10 12 14 16 18 20 22 24 26 28 32 64 128
+    awk '{print $1, "=", $2}' $rcl/${spp}.txt > $rcl/${spp}_rcl.txt
 
-        ### create cloud-optimised tif
-        gdal_translate $FILE   $DIR/cog_layers/${basin}/COG_${filename}  \
-            -co TILED=YES -co COPY_SRC_OVERVIEWS=YES -co COMPRESS=LZW 
-    done
+    r.reclass --o input=subcat output=$outn rules=$rcl/${spp}_rcl.txt
+
+    r.mapcalc --o "${outn}_msk = if(maskt == 1, $outn, null())"
+
+    r.out.gdal --o -f -c -m  createopt="COMPRESS=DEFLATE,ZLEVEL=9" type=Byte \
+        format=GTiff nodata=255 input=${outn}_msk output=$out/${outn}
+
 done
+
+
+for sp in $(ls $out | awk -F_ 'BEGIN{OFS="_";}{print $1, $2}' | sort | uniq)
+do
+
+    [[ -f $out/${sp}_diff.tif ]] && continue
+    r.mapcalc --o "${sp}_diff = ${sp}_f_prob.tif_msk - ${sp}_c_prob.tif_msk"
+
+    r.out.gdal --o -f -c -m  createopt="COMPRESS=DEFLATE,ZLEVEL=9" type=Int16 \
+        format=GTiff nodata=-9999 input=${sp}_diff output=$out/${sp}_diff.tif
+done
+
+## sum up all the files with the difference between future and present
+test=( $(g.list rast | grep "_diff") )
+printf -v testn '%s + ' "${test[@]}" 
+r.mapcalc --o "all_diff = $(echo "${testn% + }")" 
+
+    r.out.gdal --o -f -c -m  createopt="COMPRESS=DEFLATE,ZLEVEL=9" type=Int16 \
+        format=GTiff nodata=-9999 input=all_diff output=$out/allspp_diff.tif
+
+
